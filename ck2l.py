bca59f56f70d86aea92949621d2e1a1ea2662e66
schliesser-p <schliesserp@gmail.com>
2021-12-18 17:54:49 +0100

    return clean list

--- ck2l.py
+++ ck2l.py
@@ -1,6 +1,6 @@
 import requests as req
 from bs4 import BeautifulSoup
-
+import re
 
 def get_recepy_list(url):
     page = req.get(url)
@@ -8,4 +8,21 @@ def get_recepy_list(url):
     result = soup_page.find(
         "table",{"class":"ingredients table-header"}
     )
-    return result
+    table_list = []
+    # every pair is one ingredient
+    for row in result.findAll("tr"):
+        ingredients = row.findAll("span")
+        table_list.extend(ingredients)
+    
+    ingredient_list = []
+    for ii in range(len(table_list)):
+        ingredient_list.append(str(table_list[ii].contents))
+
+    clean = []
+    for ingredient in ingredient_list:
+        remove_link = re.sub(r"<(?:\b[^>]*>|/a>)", "", ingredient)
+        clean.append(remove_link.replace(" ", "").replace("\\n", "").replace("\n", ""))
+
+    return clean
+
+    
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ck2l import get_recepy_list
 
 def main():
     url = "https://www.chefkoch.de/rezepte/772011180069862/Die-echte-Sauce-Bolognese.html"
-    print(get_recepy_list(url))
-
+    recepy = get_recepy_list(url)
+    print(recepy)
 if __name__ == "__main__":
     main()
