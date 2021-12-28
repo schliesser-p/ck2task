81b1172d09f46cb505d05c070cea0404261bbc7a
schliesser-p <schliesserp@gmail.com>
2021-12-28 14:33:29 +0100

    Form for api

--- api.py
+++ api.py
@@ -1,19 +1,30 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 from ck2l import get_recepy_dict
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route("/", methods=["GET"])
 def home():
-    return "<h1> API Test </h1>"
+    return """
+<html>
+   <body>
+      <form action = "http://localhost:5000/recepy" method = "post">
+         <p>Enter Chefkoch url:</p>
+         <p><input type = "text" name = "url" /></p>
+         <p><input type = "submit" value = "submit" /></p>
+      </form>   
+   </body>
+</html>
+    """
 
-url = "https://www.chefkoch.de/rezepte/745721177147257/Lasagne.html"
-recepy = get_recepy_dict(url)
+@app.route("/recepy", methods=["POST"])
+def get_recepy():
+    url = request.form["url"]
 
-@app.route("/recepy")
-def api_all():
+    recepy = get_recepy_dict(url)
     return jsonify(recepy)
-
+    
 app.run()
